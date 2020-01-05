79100a0092b9304700047c1bde559801227ca76b
Ree-jp <49682119+Ree-jp@users.noreply.github.com>
2020-01-05 15:28:31 +0900

    Update Main.php

--- src/ree/template/Main.php
+++ src/ree/template/Main.php
@@ -3,6 +3,18 @@ namespace ree\template;
 
 use pocketmine\plugin\PluginBase;
 
-class Main extends PluginBase{
+class Main extends PluginBase
+{
+    private const NOTICE = "§a>> ";
     
+    public function onEnable()
+    {
+        $this->getLogger()->info(self::NOTICE.'読み込みました');
+        parent::onEnable();
+    }
+    
+    public function onDisable()
+    {
+        parent::onDisable();
+    }
 }
